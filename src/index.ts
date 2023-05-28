048395b2bd971fc049f84ca78093cb11f2e0df33
leonardo <leonardogarreto@gmail.com>
2023-05-28 16:50:57 -0300

    initial commint

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+app.get("/recipes", async (req, res) => {
+  try {
+    const recipes = await prisma.recipe.findMany();
+    res.json(recipes);
+  } catch (error) {
+    console.error("Error retrieving recipes:", error);
+    res.status(500).send("An error occurred while retrieving recipes.");
+  }
+});
+
+app.get("/recipes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const recipe = await prisma.recipe.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!recipe) {
+      return res.status(404).send("Recipe not found");
+    }
+
+    res.json(recipe);
+  } catch (error) {
+    console.error("Error retrieving recipe:", error);
+    res.status(500).send("An error occurred while retrieving the recipe.");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
